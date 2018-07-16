f3a7c8bffbdd790ffb3c43241f95b20252145db4
Ron <li.rongcheng@gmail.com>
2018-07-16 16:59:41 +1000

    add local package corecmd

--- FsharpConsoleApp/Program.fs
+++ FsharpConsoleApp/Program.fs
@@ -1,8 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open CoreCmd
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let cmd = new CoreCmd.CommandExecutor()
+    cmd.Execute(argv)
     0 // return an integer exit code
