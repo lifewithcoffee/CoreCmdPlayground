bed92f7fda9a758de033643ea439e51bdb5a5c24
Ron <li.rongcheng@gmail.com>
2018-07-17 23:46:28 +1000

    test c# 7 feature

--- CoreCmdPlayground/Commands/StaticClassToReplaceNamespace.cs
+++ CoreCmdPlayground/Commands/StaticClassToReplaceNamespace.cs
@@ -1,18 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 static class StaticClassToReplaceNamespace
 {
     static public Action Foo()
     {
-        return foo = () => Console.WriteLine("hello");
+        return () => Console.WriteLine("hello");
     }
 
-    static Action foo = () => Console.WriteLine("hello");
+    static Action foo1 = () => Console.WriteLine("hello");
+    static Action foo2 = () => { Console.WriteLine("hello"); };
+    static void foo3() => Console.WriteLine("hello");
 
     class Class2
     {
+        public void Foo() => Console.WriteLine("hello");
+        Func<int, int, string> Foo2 = (int x, int y) => { Console.WriteLine($"hello {x}, {y}"); return "done"; };
+        Predicate<int> Foo3 = (int x) => { Console.WriteLine($"hello {x}"); return false; };
 
+        string GetText(string path, string filename)
+        {
+            var sr = File.OpenText(AppendPathSeparator(path) + filename);
+            var text = sr.ReadToEnd();
+            return text;
+
+            // Declare a local function.
+            string AppendPathSeparator(string filepath)
+            {
+                if (!filepath.EndsWith(@"\"))
+                    filepath += @"\";
+
+                return filepath;
+            }
+        }
     }
 }
