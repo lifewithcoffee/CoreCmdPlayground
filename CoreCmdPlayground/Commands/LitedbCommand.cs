72222fa6474e8cd0a1a1f13ec6041eb865b1519b
Ron <li.rongcheng@gmail.com>
2020-05-13 03:30:10 +1000

    add litedb demo

---
+++ CoreCmdPlayground/Commands/LitedbCommand.cs
@@ -0,0 +1,16 @@
+﻿using LiteDBTestLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UtilityLib;
+
+namespace CoreCmdPlayground.Commands
+{
+    public class LitedbCommand
+    {
+        public void Test1()
+        {
+            new LiteDBOperator().Test1();
+        }
+    }
+}
---
+++ LiteDBTestLib/LiteDBOperator.cs
@@ -0,0 +1,54 @@
+﻿using LiteDB;
+using LiteDBTestLib.Modules;
+using System;
+using System.Linq;
+
+namespace LiteDBTestLib
+{
+    public class LiteDBOperator
+    {
+        public void Test1()
+        {
+            Console.WriteLine("LiteDBOperator.Test1() called.");
+
+            using (var db = new LiteDatabase(@"C:\_temp\MyData.db"))
+            {
+                // Get a collection (or create, if doesn't exist)
+                var col = db.GetCollection<Customer>("customers");
+
+                // Create your new customer instance
+                var customer = new Customer
+                {
+                    Name = "John Doe",
+                    Phones = new string[] { "8000-0000", "9000-0000" },
+                    IsActive = true
+                };
+
+                // Insert new customer document (Id will be auto-incremented)
+                col.Insert(customer);
+
+                // Update a document inside a collection
+                customer.Name = "Jane Doe";
+
+                col.Update(customer);
+
+                // Index document using document Name property
+                col.EnsureIndex(x => x.Name);
+
+                // Use LINQ to query documents (filter, sort, transform)
+                var results = col.Query()
+                    .Where(x => x.Name.StartsWith("J"))
+                    .OrderBy(x => x.Name)
+                    .Select(x => new { x.Name, NameUpper = x.Name.ToUpper() })
+                    .Limit(10)
+                    .ToList();
+
+                // Let's create an index in phone numbers (using expression). It's a multikey index
+                col.EnsureIndex(x => x.Phones);
+
+                // and now we can query phones
+                var r = col.FindOne(x => x.Phones.Contains("8888-5555"));
+            }
+        }
+    }
+}
