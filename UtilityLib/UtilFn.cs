121b92ed4ab9a437422c6e2f7c1f7b31d8e0665c
Ron <li.rongcheng@gmail.com>
2020-05-12 20:32:04 +1000

    add UtilityLib project

---
+++ UtilityLib/UtilFn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UtilityLib
+{
+    public static class Print
+    {
+        public static void Separator_______________________________________()
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(1);        // get caller method's frame info
+
+            var currentMethodName = sf.GetMethod();
+            Console.WriteLine($"----------------------------------------------{currentMethodName.Name}");
+        }
+
+    }
+}
