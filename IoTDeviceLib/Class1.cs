1aba32713da8ded6eeb8da26b0ecf08f93b06842
Ron <li.rongcheng@gmail.com>
2020-10-19 16:35:00 +1100

    add test project for Iot.Device.Bindings and System.Device.Gpio

---
+++ IoTDeviceLib/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Iot.Device.Spi;
+using System;
+using System.Device.Spi;
+
+namespace IoTDeviceLib
+{
+    public class Class1
+    {
+        public void SpiTest()
+        {
+            using (SpiDevice spi = new SoftwareSpi(clk: 6, miso: 23, mosi: 5, cs: 24))
+            {
+                // do stuff over SPI
+            }
+        }
+    }
+}
