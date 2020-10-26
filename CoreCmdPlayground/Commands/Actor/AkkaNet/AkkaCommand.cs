ab580989304c9041ab1d5b2eefd56434ac99aabd
Ron <li.rongcheng@gmail.com>
2020-10-27 10:12:39 +1100

    Separate akka.net remote commands

--- CoreCmdPlayground/Commands/Actor/AkkaNet/AkkaCommand.cs
+++ CoreCmdPlayground/Commands/Actor/AkkaNet/AkkaCommand.cs
@@ -1,5 +1,5 @@
 ﻿using Akka.Actor;
-using Akka.Configuration;
+using CoreCmd.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,7 +19,7 @@ namespace CoreCmdPlayground.Commands.Actor.AkkaNet
             // create an actor and get its reference
             var greeter = system.ActorOf<GreetingActor>("greeter");
 
-            for(int i = 0; i < 1000; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 // send message to the target actor
                 greeter.Tell(new Greet($"greeting {i}"));
@@ -27,69 +27,6 @@ namespace CoreCmdPlayground.Commands.Actor.AkkaNet
 
             Thread.Sleep(1200);
             //Console.ReadKey();
-        }
-
-        public void StartDeployTarget()
-        {
-            try
-            {
-                using (var system = ActorSystem.Create("DeployTarget", ConfigurationFactory.ParseString(@"akka {
-                        actor.provider = remote
-                        remote {
-                            dot-netty.tcp {
-                                port = 8090
-                                hostname = localhost
-                            }
-                        }
-                    }")))
-                {
-                    Console.ReadKey();
-                }
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-        }
-
-        public void StartDeployer()
-        {
-            using (var system = ActorSystem.Create("Deployer", ConfigurationFactory.ParseString(@"
-            akka {  
-                actor{
-                    provider = remote
-                    deployment {
-                        /remoteecho {
-                            remote = ""akka.tcp://DeployTarget@localhost:8090""
-                        }
-                    }
-                }
-                remote {
-                    dot-netty.tcp {
-                        port = 0
-                        hostname = localhost
-                    }
-                }
-            }")))
-            {
-                var remoteAddress = Address.Parse("akka.tcp://DeployTarget@localhost:8090");
-
-                //deploy remotely via config
-                var remoteEcho1 = system.ActorOf(Props.Create(() => new EchoActor()), "remoteecho");
-
-                //deploy remotely via code
-                var remoteEcho2 =
-                    system.ActorOf(
-                        Props.Create(() => new EchoActor())
-                            .WithDeploy(Deploy.None.WithScope(new RemoteScope(remoteAddress))), "coderemoteecho");
-
-
-                // create local actors
-                system.ActorOf(Props.Create(() => new HelloActor(remoteEcho1)));
-                system.ActorOf(Props.Create(() => new HelloActor(remoteEcho2)));
-
-                Console.ReadKey();
-            }
         }
     }
 }
---
+++ CoreCmdPlayground/Commands/Actor/AkkaNet/AkkaRemoteCommand.cs
@@ -0,0 +1,72 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using System;
+
+namespace CoreCmdPlayground.Commands.Actor.AkkaNet
+{
+    class AkkaRemoteCommand
+    { 
+        public void DeployRemote()
+        {
+            try
+            {
+                using (var system = ActorSystem.Create("DeployTarget", ConfigurationFactory.ParseString(@"akka {
+                        actor.provider = remote
+                        remote {
+                            dot-netty.tcp {
+                                port = 8090
+                                hostname = localhost
+                            }
+                        }
+                    }")))
+                {
+                    Console.ReadKey();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void StartDeployer()
+        {
+            using (var system = ActorSystem.Create("Deployer", ConfigurationFactory.ParseString(@"
+            akka {  
+                actor{
+                    provider = remote
+                    deployment {
+                        /remoteecho {
+                            remote = ""akka.tcp://DeployTarget@localhost:8090""
+                        }
+                    }
+                }
+                remote {
+                    dot-netty.tcp {
+                        port = 0
+                        hostname = localhost
+                    }
+                }
+            }")))
+            {
+                var remoteAddress = Address.Parse("akka.tcp://DeployTarget@localhost:8090");
+
+                //deploy remotely via config
+                var remoteEcho1 = system.ActorOf(Props.Create(() => new EchoActor()), "remoteecho");
+
+                //deploy remotely via code
+                var remoteEcho2 =
+                    system.ActorOf(
+                        Props.Create(() => new EchoActor())
+                            .WithDeploy(Deploy.None.WithScope(new RemoteScope(remoteAddress))), "coderemoteecho");
+
+
+                // create local actors
+                system.ActorOf(Props.Create(() => new HelloActor(remoteEcho1)));
+                system.ActorOf(Props.Create(() => new HelloActor(remoteEcho2)));
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
