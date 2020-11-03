c0ede882172505bc3bda7aab49d97f3237697d03
Ron <li.rongcheng@gmail.com>
2020-11-04 01:59:10 +1100

    clean code

--- CoreCmdPlayground/Commands/MediatR/PipelineBehaviors/RequestValidationBehavior.cs
+++ CoreCmdPlayground/Commands/MediatR/PipelineBehaviors/RequestValidationBehavior.cs
@@ -74,38 +74,4 @@ namespace CoreCmdPlayground.Commands.MediatR.PipelineBehaviors
             }
         }
     }
-
-    /** [MCN] This is a global exception filter */
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
-    {
-        public override void OnException(ExceptionContext context)
-        {
-            Console.WriteLine("$--------- OnException() called ---------$");
-
-            if (context.Exception is MyValidationException)
-            {
-                context.HttpContext.Response.ContentType = "application/json";
-                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                context.Result = new JsonResult(((MyValidationException)context.Exception).Failures);
-
-                return;
-            }
-
-            var code = HttpStatusCode.InternalServerError;
-
-            // if (context.Exception is NotFoundException)
-            // {
-            //     code = HttpStatusCode.NotFound;
-            // }
-
-            context.HttpContext.Response.ContentType = "application/json";
-            context.HttpContext.Response.StatusCode = (int)code;
-            context.Result = new JsonResult(new
-            {
-                error = new[] { context.Exception.Message },
-                stackTrace = context.Exception.StackTrace
-            });
-        }
-    }
 }
