3ab36c9fe9a26735f9fa5935b2da0c7eef307807
Ron <li.rongcheng@gmail.com>
2020-11-11 16:35:52 +1100

    add package refit to corecmdplayground.rest

---
+++ CoreCmdPlayground.Rest/Commands/RefitCommand.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCmdPlayground.Rest.Commands
+{
+    public interface IGitHubApi
+    {
+        [Get("/users/{userid}")]
+        Task<User> GetUser(string userid);
+    }
+
+    class RefitCommand
+    {
+    }
+}
