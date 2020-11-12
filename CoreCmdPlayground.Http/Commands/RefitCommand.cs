2e05714829a8f1e7407ba587be746ab6283b0f39
Ron <li.rongcheng@gmail.com>
2020-11-12 18:16:35 +1100

    implement refit github-user command

--- CoreCmdPlayground.Http/Commands/RefitCommand.cs
+++ CoreCmdPlayground.Http/Commands/RefitCommand.cs
@@ -1,12 +1,30 @@
-﻿using Refit;
+﻿using CoreCmdPlayground.Http.Refit;
+using Refit;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CoreCmdPlayground.Http.Commands
 {
     class RefitCommand
     {
+        public async Task GithubUser(string username)
+        {
+            try
+            {
+                var gitHubApi = RestService.For<IGitHubApi>("https://api.github.com");
+                var user = await gitHubApi.GetUser(username);
+
+                Console.WriteLine(JsonSerializer.Serialize(user, new JsonSerializerOptions { WriteIndented = true }));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
     }
 }
---
+++ CoreCmdPlayground.Http/Refit/Github.cs
@@ -0,0 +1,37 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCmdPlayground.Http.Refit
+{
+    [Headers("User-Agent: .NET Foundation Repository Reporter")]    // must have, otherwise will return 403 forbidden
+    public interface IGitHubApi
+    {
+        [Get("/users/{user}")]
+        Task<GithubUser> GetUser(string user);
+    }
+
+    /// <summary>
+    /// Doc: https://developer.github.com/v3/users/
+    /// </summary>
+    public class GithubUser
+    {
+        public string login { get; set; }
+
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public string blog { get; set; }
+
+        public string company { get; set; }
+
+        public string location { get; set; }
+
+        public string bio { get; set; }
+
+        public string avatar_url { get; set; }
+    }
+}
