b18f35d3e763660e7cf8761fd441904c602dfe73
Ron <li.rongcheng@gmail.com>
2021-04-16 15:34:40 +1000

    upgrade CoreCmdPlayground to .net5.0 & CoreCmd to v1.6

--- CoreCmdPlayground/do.cmd
+++ CoreCmdPlayground/do.cmd
@@ -3,7 +3,7 @@
 ::dotnet run %*
 
 :: =========================================================================
-SET DEBUG_PATH=%CD%\bin\Debug\netcoreapp3.1
+SET DEBUG_PATH=%CD%\bin\Debug\net5.0
 dotnet %DEBUG_PATH%\corecmdplayground.dll %*
 
 :: =========================================================================
